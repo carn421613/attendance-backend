7a482f963b43e4c05f3bfe653dfd758bafec7bcd
carn421613 <carn421613@gmail.com>
2026-03-30 18:27:29 +0530

    lECTANALY

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+
 app.use(express.static(path.join(__dirname, "../student")));
 // ✅ Node fetch (needed for Python service calls)
 const fetch = (...args) =>
